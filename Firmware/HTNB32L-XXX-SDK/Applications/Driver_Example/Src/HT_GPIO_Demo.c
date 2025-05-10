f8888748404d00137644e87eaf49da71d900f2f1
Solano Lima <solanolimma@gmail.com>
2025-05-10 09:27:35 -0400

    atividade pratica 3

--- Firmware/HTNB32L-XXX-SDK/Applications/Driver_Example/Src/HT_GPIO_Demo.c
+++ Firmware/HTNB32L-XXX-SDK/Applications/Driver_Example/Src/HT_GPIO_Demo.c
@@ -13,114 +13,48 @@
  *
  */
 
-#include "HT_GPIO_Demo.h"
+#include "main.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
-static volatile uint8_t gpio_exti = 0;
+static uint32_t uart_cntrl = (ARM_USART_MODE_ASYNCHRONOUS | ARM_USART_DATA_BITS_8 | ARM_USART_PARITY_NONE | 
+                                ARM_USART_STOP_BITS_1 | ARM_USART_FLOW_CONTROL_NONE);
 
-/*!******************************************************************
- * \fn static void HT_GPIO_Callback(void)
- * \brief GPIO callback. Function that will be called after aa GPIO IRQn
- * be detected by the device.
- *
- * \param[in]  none
- * \param[out] none
- *
- * \retval none.
- *******************************************************************/
-static void HT_GPIO_Callback(void);
+extern USART_HandleTypeDef huart1;
 
-/*!******************************************************************
- * \fn static void HT_GPIO_InitButton(void)
- * \brief Initialize GPIO button.
- *
- * \param[in]  none
- * \param[out] none
- *
- * \retval none.
- *******************************************************************/
-static void HT_GPIO_InitButton(void);
 
-/*!******************************************************************
- * \fn static void HT_GPIO_InitLed(void)
- * \brief Initialize GPIO LED.
- *
- * \param[in]  none
- * \param[out] none
- *
- * \retval none.
- *******************************************************************/
-static void HT_GPIO_InitLed(void);
-
-static void HT_GPIO_Callback(void) {
-  //Save current irq mask and diable whole port interrupts to get rid of interrupt overflow
-  uint16_t portIrqMask = GPIO_SaveAndSetIRQMask(BUTTON_INSTANCE);
-
-  if (HT_GPIO_GetInterruptFlags(BUTTON_INSTANCE) & (1 << BUTTON_PIN)) {
-      gpio_exti ^= 1;
-      HT_GPIO_ClearInterruptFlags(BUTTON_INSTANCE, 1 << BUTTON_PIN);
-      delay_us(100000);
-  }
-
-  HT_GPIO_RestoreIRQMask(BUTTON_INSTANCE, portIrqMask);
+void Task1(void *pvParameters) {
+    while (1) {
+        printf("Tarefa 1 executando...\n");
+        vTaskDelay(pdMS_TO_TICKS(500));         // Delay de 500ms
+    }
 }
 
-static void HT_GPIO_InitButton(void) {
-  GPIO_InitType GPIO_InitStruct = {0};
-
-  GPIO_InitStruct.af = PAD_MuxAlt0;
-  GPIO_InitStruct.pad_id = BUTTON_PAD_ID;
-  GPIO_InitStruct.gpio_pin = BUTTON_PIN;
-  GPIO_InitStruct.pin_direction = GPIO_DirectionInput;
-  GPIO_InitStruct.pull = PAD_InternalPullUp;
-  GPIO_InitStruct.instance = BUTTON_INSTANCE;
-  GPIO_InitStruct.exti = GPIO_EXTI_ENABLE;
-  GPIO_InitStruct.interrupt_config = GPIO_InterruptFallingEdge;
-
-  HT_GPIO_Init(&GPIO_InitStruct);
-
-  // Enable IRQ
-  HT_XIC_SetVector(PXIC_Gpio_IRQn, HT_GPIO_Callback);
-  HT_XIC_EnableIRQ(PXIC_Gpio_IRQn);
+void Task2(void *pvParameters) {
+    while (1) {
+        printf("Tarefa 2 executando...\n");
+        vTaskDelay(pdMS_TO_TICKS(1000)); // Delay de 1000ms
+    }
 }
 
-static void HT_GPIO_InitLed(void) {
-  GPIO_InitType GPIO_InitStruct = {0};
+/**
+  \fn          int main_entry(void)
+  \brief       main entry function.
+  \return
+*/
+void main_entry(void) {
+    HAL_USART_InitPrint(&huart1, GPR_UART1ClkSel_26M, uart_cntrl, 115200);
+    printf("Exemplo FreeRTOS\n");
 
-  GPIO_InitStruct.af = PAD_MuxAlt0;
-  GPIO_InitStruct.pad_id = LED_PAD_ID;
-  GPIO_InitStruct.gpio_pin = LED_GPIO_PIN;
-  GPIO_InitStruct.pin_direction = GPIO_DirectionOutput;
-  GPIO_InitStruct.init_output = 0;
-  GPIO_InitStruct.pull = PAD_AutoPull;
-  GPIO_InitStruct.instance = LED_INSTANCE;
-  GPIO_InitStruct.exti = GPIO_EXTI_DISABLED;
+    xTaskCreate(Task1, "Blink", 128, NULL, 1, NULL);
+    xTaskCreate(Task2, "Print", 128, NULL, 1, NULL);
 
-  HT_GPIO_Init(&GPIO_InitStruct);
+    vTaskStartScheduler();
+    
+    printf("Nao deve chegar aqui.\n");
+
+    while(1);
+
 }
 
-void HT_GPIO_App(void) {
-
-  ht_printf("GPIO Example Start!\n");
-  
-  HT_GPIO_InitButton();
-  HT_GPIO_InitLed();
-
-  slpManNormalIOVoltSet(IOVOLT_3_30V);
-
-  while(1) {
-
-     if (gpio_exti){
-       //delay_us(20000);
-        ht_printf("botao pressionado\n");
-       HT_GPIO_WritePin(LED_GPIO_PIN, LED_INSTANCE, LED_ON);
-     }
-     else
-     {
-      //delay_us(20000);
-        ht_printf("botao solto\n");
-         HT_GPIO_WritePin(LED_GPIO_PIN, LED_INSTANCE, LED_OFF);
-     }   
-  }
-}
-
-/************************ HT Micron Semicondutores S.A *****END OF FILE****/
+/******** HT Micron Semicondutores S.A **END OF FILE*/
